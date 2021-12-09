e7845b928eca3737744fd7b13742f712068210c9
Igor <leonidlee777@gmail.com>
2021-12-09 10:55:40 +0200

    add actions

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 import $ from './lib/lib';
 
 $('button').on('click', function() {
-    $(this).hide().show().toggleClass('active').toggleAttributes('first', 'hello').getAttributes('first');
+    $('div').eq(1).toggleClass('active');
 });
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.some'));
